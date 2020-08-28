fabdef87db63b540dfa818ed63e28c1df80dc701
Zeeppss <69776277+Zeeppss@users.noreply.github.com>
2020-08-28 18:08:02 +0700

    Update Main.java

--- src/me/dev/Main.java
+++ src/me/dev/Main.java
@@ -1,19 +1,22 @@
 package me.dev;
 
-import me.dev.commands.FeedCMD;
-import me.dev.commands.GamemodeCMD;
-import me.dev.commands.HealCMD;
-import me.dev.commands.TpCMD;
+import me.dev.commands.*;
+import me.dev.listeners.BanJoinListener;
+import me.dev.listeners.EventJoinListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("JavaNotepad success to enabling...!");
+        getServer().getPluginManager().registerEvents(new EventJoinListener(), this);
+        getServer().getPluginManager().registerEvents(new BanJoinListener(), this);
         this.getCommand("heal").setExecutor(new HealCMD(this));
         this.getCommand("feed").setExecutor(new FeedCMD(this));
         this.getCommand("tp").setExecutor(new TpCMD(this));
         this.getCommand("gamemode").setExecutor(new GamemodeCMD(this));
+        this.getCommand("fly").setExecutor(new FlyCMD(this));
+        this.getCommand("ban").setExecutor(new BanCMD(this));
     }
 
     public void onDisable() {

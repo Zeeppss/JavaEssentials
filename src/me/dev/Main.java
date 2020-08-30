1414b12eaf09298f8878195e3ba780c1c278d608
Zeeppss <69776277+Zeeppss@users.noreply.github.com>
2020-08-30 09:43:27 +0700

    Update Main.java

--- src/me/dev/Main.java
+++ src/me/dev/Main.java
@@ -1,28 +1,44 @@
 package me.dev;
 
 import me.dev.commands.*;
-import me.dev.listeners.BanJoinListener;
-import me.dev.listeners.EventJoinListener;
+import me.dev.managers.GamemodeManager;
+import me.dev.managers.RtpManager;
+import me.dev.listeners.JoinEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-    @Override
+    public static String pre = "§6[§bJavaNotepad§6] | ";
+    public static String mbp = "§cPlayer Only!.";
+    public static String np = "§cYou don't have essentials permissions!";
+
+    public Main() {
+    }
+
     public void onEnable() {
-        getLogger().info("JavaNotepad success to enabling...!");
-        getServer().getPluginManager().registerEvents(new EventJoinListener(), this);
-        getServer().getPluginManager().registerEvents(new BanJoinListener(), this);
-        this.getCommand("heal").setExecutor(new HealCMD(this));
-        this.getCommand("feed").setExecutor(new FeedCMD(this));
-        this.getCommand("tp").setExecutor(new TpCMD(this));
-        this.getCommand("gamemode").setExecutor(new GamemodeCMD(this));
-        this.getCommand("fly").setExecutor(new FlyCMD(this));
-        this.getCommand("ban").setExecutor(new BanCMD(this));
-        this.getCommand("kick").setExecutor(new KickCMD(this));
-        this.getCommand("vanish").setExecutor(new VanishCMD(this));
-        this.getCommand("invsee").setExecutor(new InvseeCMD(this));
+        Bukkit.getConsoleSender().sendMessage(pre + " §7------------------------------------");
+        Bukkit.getConsoleSender().sendMessage(pre + " §aPlugin was loaded successfully");
+        Bukkit.getConsoleSender().sendMessage(pre + " §aPLugin was created by Zeeppss!");
+        Bukkit.getConsoleSender().sendMessage(pre + " §7------------------------------------");
+        this.getCommand("vanish").setExecutor(new VanishCMD());
+        this.getCommand("v").setExecutor(new VanishCMD());
+        this.getCommand("feed").setExecutor(new FeedCMD());
+        this.getCommand("heal").setExecutor(new HealCMD());
+        this.getCommand("gamemode").setExecutor(new GamemodeCMD());
+        this.getCommand("gm").setExecutor(new GamemodeCMD());
+        this.getCommand("fly").setExecutor(new FlyCMD());
+        this.getCommand("chatclear").setExecutor(new ClearChatCMD());
+        this.getCommand("rtp").setExecutor(new RtpCMD());
+        this.getCommand("tp").setExecutor(new TpCMD());
+        Bukkit.getPluginManager().registerEvents(new GamemodeManager(), this);
+        Bukkit.getPluginManager().registerEvents(new JoinEvent(), this);
+        Bukkit.getPluginManager().registerEvents(new RtpManager(), this);
     }
 
     public void onDisable() {
-        getLogger().info("JavaNotepad disabling...");
+        Bukkit.getConsoleSender().sendMessage(pre + " §7------------------------------------");
+        Bukkit.getConsoleSender().sendMessage(pre + " §cPlugin was disabled!");
+        Bukkit.getConsoleSender().sendMessage(pre + " §cPlugin was created by Zeeppss!");
+        Bukkit.getConsoleSender().sendMessage(pre + " §7------------------------------------");
     }
 }

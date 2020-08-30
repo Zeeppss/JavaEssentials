872f59e349216773e421539eec26aa76c37df49f
Zeeppss <69776277+Zeeppss@users.noreply.github.com>
2020-08-30 09:46:22 +0700

    Update HealCMD.java

--- src/me/dev/commands/HealCMD.java
+++ src/me/dev/commands/HealCMD.java
@@ -1,34 +1,47 @@
 package me.dev.commands;
 
 import me.dev.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class HealCMD implements CommandExecutor {
-    private Main plugin;
-
-    public HealCMD(Main plugin) {
-        this.plugin = plugin;
-        plugin.getCommand("heal").setExecutor(this);
-
+    public HealCMD() {
     }
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-        if (cmd.getName().equalsIgnoreCase("heal")) ;
-        Player p = (Player) sender;
-        if (args.length == 0) {
-            if (p.hasPermission("essentials.heal") && p.isOp()) {
-            }
-            p.setHealth(20.0);
-            p.setFoodLevel(20);
-            p.setFireTicks(0);
-            return true;
-        } else {
-            p.sendMessage("§cYou are lacking this permission essentials.heal");
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(Main.pre + " | §cPlayer Only!.");
         }
-        return true;
+
+        Player p = (Player)sender;
+        if (p.hasPermission("essentials.heal")) {
+            if (args.length == 0) {
+                this.healOther(p);
+            } else if (args.length == 1) {
+                Player target = Bukkit.getPlayer(args[0]);
+                if (target != null) {
+                    this.healOther(target);
+                    p.sendMessage(Main.pre + "§e" + target.getName() + "§aYou has been healed!.");
+                } else {
+                    p.sendMessage(Main.pre + "§cPlayer not found!.");
+                }
+            } else {
+                p.sendMessage(Main.pre + "§cUsage: /heal");
+            }
+        } else {
+            p.sendMessage(Main.pre + "§cYou don't have essentials.heal permissions.");
+        }
+
+        return false;
+    }
+
+    private void healOther(Player p) {
+        p.setFoodLevel(20);
+        p.setHealth(20.0D);
+        p.setFireTicks(0);
+        p.sendMessage(Main.pre + "§aYou has been healed!.");
     }
 }

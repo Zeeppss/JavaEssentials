b73e1505e153546ba8d32b36075dcb849a8254de
Zeeppss <69776277+Zeeppss@users.noreply.github.com>
2020-11-14 18:52:09 +0700

    Update and rename LightningLoginEvent.java to ThorJoinEvent.java

--- src/dev/zeeppss/javaessentials/listeners/ThorJoinEvent.java
+++ src/dev/zeeppss/javaessentials/listeners/ThorJoinEvent.java
@@ -5,14 +5,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-public class LightningLoginEvent implements Listener {
-    public LightningLoginEvent() {
+public class ThorJoinEvent implements Listener {
+    public ThorJoinEvent() {
     }
 
     @EventHandler
     public void onLogin(PlayerJoinEvent e) {
         Player player = e.getPlayer();
-        if (player.hasPermission("essentials.lightninglogin") || player.isOp()) {
+        if (player.hasPermission("essentials.thorjoin") || player.isOp()) {
             player.getWorld().strikeLightningEffect(player.getLocation());
         }
 
